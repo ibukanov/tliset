a8cbf971257953f7b203371e95cb57522477af7d
Igor Bukanov <igor@mir2.org>
2014-09-27 22:17:34 +0200

    Working install script

---
+++ install.sh
@@ -0,0 +1,195 @@
+#!/bin/sh
+
+set -u
+set -e
+
+self="$(dirname $0)"
+ssh_config=/etc/ssh/ssh_config
+auto_master=/etc/auto.master.d/tliset.autofs
+set_etc=/etc/tliset
+set_key_dir="$set_etc/ssh_keys"
+auto_map="$set_etc/automap"
+ssh_known_hosts="/var/local/sshfs_known_hosts"
+sshfs_timeout=60
+mount_dir=/set
+
+install=
+uninstall=
+show_usage=
+dry_run=
+
+loop_mount_dirs() {
+    test $# -ge 1 || err "map_dirs requires an argument"
+    "$@" bergenrabbit-photo bergenrabbit.net@www.hippyru.net:/www/site/hippy.ru/html
+    "$@" hippy.ru hippy.ru@www.hippyru.net:/www/site/hippy.ru/html
+    "$@" lubava.info lubava.info@www.hippyru.net:/www/site/lubava.info/html
+    "$@" rkino rkino:/user
+}
+
+err() {
+    echo "$0: $@" 1>&2
+    exit 1
+}
+
+usage_err() {
+    echo "$0: $@" 1>&2
+    echo "Try $0 -h for usage" 1>&2
+    exit 1
+}
+
+dump_seperator() {
+    if test -n "$dry_run"; then
+	echo "-----------------------------------------------------------------"
+    fi
+}
+
+log() {
+    if test -n "$dry_run"; then
+	echo "$@"
+    else
+	echo "$@" 1>&2
+    fi
+}
+
+cmd() {
+    local cmd
+    cmd="$1"
+    shift
+    dump_seperator
+    log "cmd $cmd $@"
+    if test -z "$dry_run"; then
+	"$cmd" "$@"
+    fi
+}
+
+rm_if_empty_dir() {
+    local dir
+    dir="$1"
+    if test -d "$dir"; then
+	cmd rmdir --ignore-fail-on-non-empty "$dir"
+    else
+	dump_seperator
+	log "skip rmdir: !test -d $dir"
+    fi
+}
+
+rm_if_exists() {
+    local file
+    file="$1"
+    if test -e "$file"; then
+	cmd rm "$file"
+    else
+	dump_seperator
+	log "skip rm: !test -e $file"
+    fi
+}
+
+new_file() {
+    local mode path
+    mode="$1"
+    path="$2"
+    rm_if_exists "$path"
+    dump_seperator
+    log "new_file $mode $path"
+    if test -z "$dry_run"; then
+	touch "$path"
+	chmod "$mode" "$path"
+    fi
+}
+
+add_line() {
+    local path text
+    path="$1"
+    text="$2"
+    if test -n "$dry_run"; then
+	echo "$text"
+    else
+	echo "$text" >> "$path"
+    fi
+}
+
+add_sshfs_dir() {
+    local dir remote s
+
+    dir="$1"
+    remote="$2"
+    s="-fstype=fuse.sshfs,"
+    s="${s}rw,nodev,nosuid,noatime,allow_other,nonempty,"
+    s="${s}max_read=65536,reconnect,workaround=all,transform_symlinks,follow_symlinks,"
+    s="${s}uid=\$UID,gid=\$GID,"
+    s="${s}IdentityFile=\$HOME/.ssh/id_rsa,"
+    s="${s}IdentityFile=\$HOME/.ssh/id_ed25519,"
+    s="${s}IdentityFile=$set_key_dir/$dir,"
+    s="${s}ServerAliveInterval=5,ServerAliveCountMax=2,"
+    s="${s}StrictHostKeyChecking=no,"
+    s="${s}UserKnownHostsFile=$ssh_known_hosts,"
+    s="${s}ControlPath=none"
+    add_line "$auto_map" "$mount_dir/$dir $s $remote"
+}
+
+do_install() {
+    if test -f "$ssh_config.orig"; then
+	cmd mv "$ssh_config" "$ssh_config.orig"
+    fi
+    cmd install -m 755 -d "$set_etc" "$set_key_dir" "$mount_dir"
+    cmd install -m 644 -T "$self/ssh_config" "$ssh_config"
+
+    new_file 644 "$auto_master"
+    add_line "$auto_master" "/- file:$auto_map --timeout=$sshfs_timeout"
+
+    new_file 644 "$auto_map"
+    loop_mount_dirs add_sshfs_dir
+
+    new_file 644 "$ssh_known_hosts"
+
+    cmd systemctl restart autofs
+}
+
+remove_mount_dir() {
+    # $2 is ugnored
+    rm_if_empty_dir "$mount_dir/$1"
+}
+
+do_uninstall() {
+    local i
+    if test -f "$ssh_config.orig"; then
+	cmd mv -f "$ssh_config.orig" "$ssh_config"
+    fi
+    rm_if_exists "$auto_map"
+    rm_if_exists "$auto_master"
+    rm_if_empty_dir "$set_key_dir"
+    rm_if_empty_dir "$set_etc"
+
+    cmd systemctl restart autofs
+
+    rm_if_exists "$ssh_known_hosts"
+    loop_mount_dirs remove_mount_dir
+    rm_if_empty_dir "$mount_dir"
+}
+
+while getopts :dihu opt; do
+    case "$opt" in
+	d ) dry_run=1 ;;
+	i ) install=1 ;;
+	h ) show_usage=1 ;;
+	u ) uninstall=1 ;;
+	/? ) usage_err "option -$OPTARG requires an argument" ;;
+	* ) usage_err "unknown -$OPTARG option" ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+test $# -eq 0 || usage_err "Unexpected extra arguments: $@"
+
+if test -z "$install" -a -z "$uninstall"; then
+    dry_run=1
+    install=1
+fi
+
+if test -n "$uninstall"; then
+    do_uninstall
+fi
+
+if test -n "$install"; then
+    do_install
+fi
